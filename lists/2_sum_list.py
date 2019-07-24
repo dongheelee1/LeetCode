bec16f8c7bc34fc231f6e7991b0bf5224a4ff1dc
donghizzle <dongheec.lee@gmail.com>
2019-07-23 22:14:59 -0400

    Update 2_sum_list.py

--- lists/2_sum_list.py
+++ lists/2_sum_list.py
@@ -1,36 +1,31 @@
-def twoSumList(nums, target): 
+'''
+1. Two Sum
 
-  '''
-  idea: 
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-  initialize a hashmap 
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-  loop through the data
+Example:
 
-    subtract current element from target 
+Given nums = [2, 7, 11, 15], target = 9,
 
-    check to see if remainder is in hashmap 
-
-    if remainder is in the hashmap 
-
-      make result[0] = current element 
-      make result[1] = remainder 
-    
-    otherwise 
-      add current element to the hashmap 
-
-  '''
-
-  hashmap = {}
-  
-  for i in range(len(nums)): 
-      
-      
-      remainder = target - nums[i] #remainder is key in the hashmap
-      
-      if remainder in hashmap: 
-          return [hashmap[remainder], i]
-      else: 
-          hashmap[nums[i]] = i 
-
-  return hashmap
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashmap = {} #key is element in nums, value is the index 
+        
+        
+        for i in range(len(nums)): 
+            
+            remainder = target - nums[i]
+            
+            if remainder in hashmap:
+            
+                return [hashmap[remainder], i]
+            
+            else: 
+                
+                hashmap[nums[i]] = i 
