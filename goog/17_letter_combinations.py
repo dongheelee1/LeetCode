8f0f941d856135fcc2eb6ffcd9efd9502c0e6215
donghizzle <dongheec.lee@gmail.com>
2019-08-02 21:48:57 -0400

    Update 17_letter_combinations.py

--- goog/17_letter_combinations.py
+++ goog/17_letter_combinations.py
@@ -16,6 +16,14 @@ Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 
 
+
+Complexity Analysis
+
+Time complexity : O(3^N * 4^M) where N is the number of digits in the input that maps to 3 letters (e.g. 2, 3, 4, 5, 6, 8) 
+and M is the number of digits in the input that maps to 4 letters (e.g. 7, 9), and N+M is the total number digits in the input.
+
+Space complexity : O(3^N * 4^M) solutions.
+
 '''
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
