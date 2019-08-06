478aadf9c00a571378e4c726c38e901362f1ff5a
donghizzle <dongheec.lee@gmail.com>
2019-08-05 23:17:48 -0400

    Create 451_sort_by_freq.py

---
+++ goog/451_sort_by_freq.py
@@ -0,0 +1,72 @@
+'''
+451. Sort Characters By Frequency
+Medium
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+'''
+class Solution:
+    
+    def frequencySort(self, s: str) -> str:
+        
+        '''
+        idea: 
+        
+        get hashmap of the frequency 
+        
+        add everything in the hashmap to the maxheap 
+        
+        build string based off of the maxheap 
+        '''
+        from collections import OrderedDict 
+        hashmap = OrderedDict()
+        
+        for char in s: 
+            if char not in hashmap: 
+                hashmap[char] = 1
+            else: 
+                hashmap[char] += 1
+                
+
+        hashmap = sorted(hashmap.items(), key=lambda t: (-t[1], t[0]))
+        #The negation of x[1] is needed to sort the characters by freq in descending first and sort characters in ascending order
+        res = ""
+        for item in hashmap: 
+            char = item[0]
+            freq = item[1]
+            while freq > 0: 
+                res += char
+                freq -= 1
+        return res 
