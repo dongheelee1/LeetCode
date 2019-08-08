b1de922b12159b71d2f249beb1158d358ea036a7
donghizzle <dongheec.lee@gmail.com>
2019-08-07 21:43:23 -0400

    Create 520_detect_capital.py

---
+++ goog/520_detect_capital.py
@@ -0,0 +1,45 @@
+'''
+520. Detect Capital
+
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+ 
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+'''
+class Solution:
+    
+    def detectCapitalUse(self, word: str) -> bool:
+        
+        #conditions? 
+        #1. all capital 
+        #2. all lowercase 
+        #3. first character is lowercase 
+        
+        #go through the entire word and count the number of capitals
+        capitals = 0 
+        for i in range(len(word)):
+            
+            if word[i].isupper(): 
+                capitals += 1 
+                
+        return len(word) == capitals or (capitals == 1 and word[0].isupper()) or capitals == 0 
+        
