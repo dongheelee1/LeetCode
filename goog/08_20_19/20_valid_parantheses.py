67019fc10d4882569446dd57d742992e6e8ba93c
donghizzle <dongheec.lee@gmail.com>
2019-09-08 17:15:39 -0400

    Update 20_valid_parantheses.py

--- goog/08_20_19/20_valid_parantheses.py
+++ goog/08_20_19/20_valid_parantheses.py
@@ -30,6 +30,28 @@ Example 5:
 Input: "{[]}"
 Output: true
 '''
+
+'''
+Idea: 
+
+If length of the string is not even, then it means that there is a bracket for which there is no match. Therefore, return True. 
+
+If the length of the string is even, then we want to initialize a stack that will contain all the open brackets. 
+
+Now, go through character by character in string. 
+
+In the case that we encounter a closed bracket, we want to check if the stack is empty or not. 
+    If the stack has elements inside, pop off the open bracket from the stack and compare it with the closed bracket. 
+        If the closed bracket is not corresponding to the open, then we want to return False 
+        Otherwise, don't 
+    If the stack doesn't have elements inside, we want to return False.
+    
+After we finish going through the string, check that the stack is empty. 
+
+If it is empty, then return True 
+If it is not empty, then there is a bracket for which there is not a match. Return False. 
+
+'''
 class Solution:
     def isValid(self, s: str) -> bool:
         
